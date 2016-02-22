627bcbb81742f52e2fa665d3f3efc4cc119ea515
Offir Golan <offirgolan@gmail.com>
2016-02-22 12:42:20 -0800

    Initial gh-pages release

---
+++ docs/api.js
@@ -0,0 +1,25 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Column",
+        "Light-Table",
+        "Row",
+        "Table"
+    ],
+    "modules": [
+        "Classes",
+        "Components"
+    ],
+    "allModules": [
+        {
+            "displayName": "Classes",
+            "name": "Classes"
+        },
+        {
+            "displayName": "Components",
+            "name": "Components"
+        }
+    ],
+    "elements": []
+} };
+});
