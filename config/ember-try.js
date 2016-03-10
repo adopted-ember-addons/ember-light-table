8c17a10099e748f08d6e8d48126dd26c5dc33ba6
Offir Golan <offirgolan@yahoo-inc.com>
2016-03-10 14:42:58 -0800

    upgrade dependencies

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,43 +1,54 @@
 /*jshint node:true*/
 module.exports = {
+  command: 'ember test',
   scenarios: [{
     name: 'default',
-    dependencies: {}
+    bower: {
+      dependencies: {}
+    }
   }, {
     name: 'ember-2.3',
-    dependencies: {
-      'ember': '~2.3.0',
-      'ember-data': '~2.3.0'
+    bower: {
+      dependencies: {
+        'ember': '~2.3.0',
+        'ember-data': '~2.3.0'
+      }
     }
   }, {
     name: 'ember-release',
-    dependencies: {
-      'ember': 'components/ember#release',
-      'ember-data': 'components/ember-data#release'
-    },
-    resolutions: {
-      'ember': 'release',
-      'ember-data': 'release'
+    bower: {
+      dependencies: {
+        'ember': 'components/ember#release',
+        'ember-data': 'components/ember-data#release'
+      },
+      resolutions: {
+        'ember': 'release',
+        'ember-data': 'release'
+      }
     }
   }, {
     name: 'ember-beta',
-    dependencies: {
-      'ember': 'components/ember#beta',
-      'ember-data': 'components/ember-data#beta'
-    },
-    resolutions: {
-      'ember': 'beta',
-      'ember-data': 'beta'
+    bower: {
+      dependencies: {
+        'ember': 'components/ember#beta',
+        'ember-data': 'components/ember-data#beta'
+      },
+      resolutions: {
+        'ember': 'beta',
+        'ember-data': 'beta'
+      }
     }
   }, {
     name: 'ember-canary',
-    dependencies: {
-      'ember': 'components/ember#canary',
-      'ember-data': 'components/ember-data#canary'
-    },
-    resolutions: {
-      'ember': 'canary',
-      'ember-data': 'canary'
+    bower: {
+      dependencies: {
+        'ember': 'components/ember#canary',
+        'ember-data': 'components/ember-data#canary'
+      },
+      resolutions: {
+        'ember': 'canary',
+        'ember-data': 'canary'
+      }
     }
   }]
 };
