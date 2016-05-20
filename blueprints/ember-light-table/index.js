3d3415d0469bfe4cc86ab25c295444225d62b06d
offirgolan <offirgolan@gmail.com>
2016-05-20 16:05:45 -0700

    Move ember-wormhole as a dependency

--- blueprints/ember-light-table/index.js
+++
@@ -1,8 +0,0 @@
-/*jshint node:true*/
-module.exports = {
-  normalizeEntityName: function() {},
-
-  afterInstall: function() {
-    return this.addAddonToProject('ember-wormhole', '0.3.5');
-  }
-};
