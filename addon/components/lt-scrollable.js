f1f35359711e70364b653ec59ded30e3577c664d
offirgolan <offirgolan@gmail.com>
2016-06-15 15:50:56 -0700

    Recalculate scrollbar when viewport dimensions change

--- addon/components/lt-scrollable.js
+++ addon/components/lt-scrollable.js
@@ -1,5 +1,31 @@
 import ScrollableComponent from 'ember-scrollable/components/scrollable';
 
 export default ScrollableComponent.extend({
-  classNames: ['lt-scrollable']
+  classNames: ['lt-scrollable'],
+
+  didInsertElement() {
+    this._super(...arguments);
+    this._updateViewportProps();
+  },
+
+  didUpdate() {
+    this._super(...arguments);
+    if(this._elementWidth !== this._getElementWidth() || this._elementHeight !== this._getElementHeight()) {
+      this._updateViewportProps();
+      this.send('recalculate');
+    }
+  },
+
+  _updateViewportProps() {
+    this._elementWidth = this._getElementWidth();
+    this._elementHeight = this._getElementHeight();
+  },
+
+  _getElementWidth() {
+    return this.$().css('width');
+  },
+
+  _getElementHeight() {
+    return this.$().css('height');
+  }
 });
