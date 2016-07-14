723f866efe645cc3984292864e671754fc65f2fd
offirgolan <offirgolan@gmail.com>
2016-07-14 16:19:24 +0300

    Update changelog

--- config/release.js
+++ config/release.js
@@ -1,9 +1,11 @@
 /* jshint node:true */
 /* var RSVP = require('rsvp'); */
 var execSync = require('child_process').execSync;
+var generateChangelog = require('ember-cli-changelog/lib/tasks/release-with-changelog');
 
 module.exports = {
   publish: true,
+  beforeCommit: generateChangelog,
   afterPublish: function(project, versions) {
     // Publish dummy app with docs to gh-pages
     runCommand('ember github-pages:commit --message "Released ' + versions.next + '"');
