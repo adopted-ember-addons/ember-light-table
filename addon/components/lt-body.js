3f268998d1c3cf9a8cd7a23deefcdd92473a0b95
Jan Buschtöns <buschtoens@gmail.com>
2016-11-14 12:17:25 +0100

    fix lonely-if and wrong property access in lt-body

--- addon/components/lt-body.js
+++ addon/components/lt-body.js
@@ -359,7 +359,7 @@ export default Component.extend({
 
   toggleExpandedRow(row) {
     let multi = this.get('multiRowExpansion');
-    let shouldExpand = !row.expanded;
+    let shouldExpand = !row.get('expanded');
 
     if (multi) {
       row.toggleProperty('expanded');
@@ -403,10 +403,8 @@ export default Component.extend({
           }
         }
         this._prevSelectedIndex = currIndex;
-      } else {
-        if (this.get('canExpand') && this.get('expandOnClick')) {
-          this.toggleExpandedRow(row);
-        }
+      } else if (this.get('canExpand') && this.get('expandOnClick')) {
+        this.toggleExpandedRow(row);
       }
 
       callAction(this, 'onRowClick', ...arguments);
