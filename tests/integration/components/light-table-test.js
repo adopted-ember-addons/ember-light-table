fdcd31e2d3b3d9f00ed87a812a3a74c8598c5866
Erick Ruiz de Chavez <eruizdechavez@fastmail.com>
2017-04-28 17:47:47 -0500

    Fix broken onScroll handler after v1.8.6 (#397)
    * Fix broken onScroll handler after v1.8.6
    
    After fixing deprecation message for onScroll, a typo was
    introduced by using `onScrollToY` instead of `onScrollY`. This
    PR resolves this problem by using the correct event handler and
    adds a basic test to ensure the event gets fired properly.
    
    * Test scroll value too
    
    Instead of just testing event handler being called, also test
    the value of the actual scrolling.

--- tests/integration/components/light-table-test.js
+++ tests/integration/components/light-table-test.js
@@ -189,3 +189,30 @@ test('passed in components can have computed properties', function(assert) {
 
   assert.equal(this.$('.custom-row.is-active').length, 0, 'none of the items are active');
 });
+
+test('onScroll', function(assert) {
+  let done = assert.async();
+  let table = new Table(Columns, createUsers(10));
+  let expectedScroll = 50;
+
+  this.setProperties({
+    table,
+    onScroll(actualScroll) {
+      assert.ok(true, 'onScroll worked');
+      assert.equal(actualScroll, expectedScroll, 'scroll position is correct');
+      done();
+    }
+  });
+
+  this.render(hbs `
+    {{#light-table table height='40vh' as |t|}}
+      {{t.head fixed=true}}
+      {{t.body
+        useVirtualScrollbar=true
+        onScroll=onScroll
+      }}
+    {{/light-table}}
+  `);
+
+  this.$('.tse-scroll-content').scrollTop(expectedScroll).scroll();
+});
