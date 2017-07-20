0d6ff259270dc4118041f61b22418f1dd2428fed
Jan Buschtöns <buschtoens@gmail.com>
2017-07-20 17:10:58 +0200

    ci/travis: use headless Chrome

--- testem.js
+++ testem.js
@@ -1,11 +1,18 @@
 /* eslint-env node */
+/* eslint-disable camelcase */
 module.exports = {
-  "test_page": "tests/index.html?coverage",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "Chrome"
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  launch_in_ci: [
+    'Chrome'
   ],
-  "launch_in_dev": [
-    "Chrome"
+  launch_in_dev: [
+    'Chrome'
   ]
 };
+
+if (process.env.HEADLESS === 'true') {
+  module.exports.browser_args = {
+    'Chrome': ['--headless', '--disable-gpu', '--remote-debugging-port=9222']
+  };
+}
