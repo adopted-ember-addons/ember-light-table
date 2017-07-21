2148a52089a85ff5209e5ef6e285cd9199dfef12
Jan Buschtöns <buschtoens@gmail.com>
2017-07-21 15:15:49 +0200

    deps: install and configure prettier as eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,19 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
+  plugins: [
+    'prettier'
+  ],
   extends: [
     'eslint:recommended',
-    'plugin:ember-suave/recommended'
+    'plugin:ember-suave/recommended',
+    'prettier'
   ],
   env: {
     browser: true
   },
   rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
     'generator-star-spacing': ['error', { before: false, after: false }],
     'ember-suave/require-access-in-comments': 'off',
     'ember-suave/no-const-outside-module-scope': 'off'
