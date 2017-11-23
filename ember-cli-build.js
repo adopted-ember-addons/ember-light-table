36149816d29f786968853b8f13c0774b1d31fe46
Jan Buschtöns <jan@buschtoens.me>
2017-11-23 08:12:45 +0100

    chore(build): include babel polyfill for demo app
    Fixes #427.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,10 @@ module.exports = function(defaults) {
     autoprefixer: {
       browsers: ['last 2 versions'],
       cascade: false
+    },
+
+    'ember-cli-babel': {
+      includePolyfill: true
     }
   });
 
