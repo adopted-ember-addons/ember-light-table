30ae851d31f2bf313bc85c5e19712e1d8007b9d5
Jan Buschtöns <jan@buschtoens.me>
2017-11-23 13:56:23 +0100

    docs(demo/occlusion): showcase isLoading problem

--- tests/dummy/mirage/scenarios/default.js
+++ tests/dummy/mirage/scenarios/default.js
@@ -3,5 +3,5 @@ export default function(server) {
   // Seed your development database using your factories. This
   // data will not be loaded in your tests.
 
-  server.createList('user', 100);
+  server.createList('user', 300);
 }
