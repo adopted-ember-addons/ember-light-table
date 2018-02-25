072bd966773dff049761ab70873541e49ccd0636
snewcomer <snewcomer24@gmail.com>
2018-02-24 16:52:32 -0800

    bring back some lt-infinity logic

--- addon/components/lt-infinity.js
+++ addon/components/lt-infinity.js
@@ -1,4 +1,6 @@
 import Component from '@ember/component';
+import { observer } from '@ember/object';
+import { run } from '@ember/runloop';
 import layout from '../templates/components/lt-infinity';
 import InViewportMixin from 'ember-in-viewport';
 
@@ -32,9 +34,41 @@ export default Component.extend(InViewportMixin, {
 
   willDestroyElement() {
     this._super(...arguments);
+    this._cancelTimers();
   },
 
   didEnterViewport() {
     this.sendAction('onScrolledToBottom');
+  },
+
+  didExitViewport() {
+    this._cancelTimers();
+  },
+
+  scheduleScrolledToBottom: observer('rows.[]', 'viewportEntered', function() {
+    // for rAF
+    if (this.get('viewportEntered')) {
+      /*
+       Continue scheduling onScrolledToBottom until no longer in viewport
+       */
+      this._scheduleScrolledToBottom();
+    }
+  }),
+
+  _scheduleScrolledToBottom() {
+    this._schedulerTimer = run.scheduleOnce('afterRender', this, this._debounceScrolledToBottom);
+  },
+
+  _debounceScrolledToBottom(delay = 100) {
+    /*
+     This debounce is needed when there is not enough delay between onScrolledToBottom calls.
+     Without this debounce, all rows will be rendered causing immense performance problems
+     */
+    this._debounceTimer = run.debounce(this, this.sendAction, 'onScrolledToBottom', delay);
+  },
+
+  _cancelTimers() {
+    run.cancel(this._schedulerTimer);
+    run.cancel(this._debounceTimer);
   }
 });
