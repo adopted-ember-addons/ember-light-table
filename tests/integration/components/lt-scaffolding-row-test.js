37c8894d8ef799649d3d597478bfb2b174d1baba
Tomasz Węgrzyn <twgrzyn1@gmail.com>
2019-10-01 21:49:33 +0200

    Test lt-scaffolding-row

--- tests/integration/components/lt-scaffolding-row-test.js
+++ tests/integration/components/lt-scaffolding-row-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render, find, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | lt-scaffolding-row', function(hooks) {
@@ -16,33 +16,33 @@ module('Integration | Component | lt-scaffolding-row', function(hooks) {
     assert.ok(find('tr'));
   });
 
-  // test('it renders <td> for each column', async function(assert) {
-  //   const columns = [{
-  //     label: 'Avatar',
-  //     valuePath: 'avatar',
-  //     width: '60px',
-  //     sortable: false,
-  //     cellComponent: 'user-avatar'
-  //   }, {
-  //     label: 'First Name',
-  //     valuePath: 'firstName',
-  //     width: '150px'
-  //   }, {
-  //     label: 'Last Name',
-  //     valuePath: 'lastName',
-  //     width: '150px'
-  //   }, {
-  //     label: 'Address',
-  //     valuePath: 'address'
-  //   }, {
-  //     label: 'State',
-  //     valuePath: 'state'
-  //   }, {
-  //     label: 'Country',
-  //     valuePath: 'country'
-  //   }];
-  //   this.set('columns', columns)
-  //   await render(hbs`{{lt-scaffolding-row columns=columns}}`);
-  //   assert.ok(this.element.tagName === 'tr');
-  // });
+  test('it renders <td> for each column', async function(assert) {
+    const columns = [{
+      label: 'Avatar',
+      valuePath: 'avatar',
+      width: '60px',
+      sortable: false,
+      cellComponent: 'user-avatar'
+    }, {
+      label: 'First Name',
+      valuePath: 'firstName',
+      width: '150px'
+    }, {
+      label: 'Last Name',
+      valuePath: 'lastName',
+      width: '150px'
+    }, {
+      label: 'Address',
+      valuePath: 'address'
+    }, {
+      label: 'State',
+      valuePath: 'state'
+    }, {
+      label: 'Country',
+      valuePath: 'country'
+    }];
+    this.set('columns', columns);
+    await render(hbs`{{lt-scaffolding-row columns=columns}}`);
+    assert.equal(findAll('td').length, columns.length);
+  });
 });
