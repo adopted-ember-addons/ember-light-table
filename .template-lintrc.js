fd592a7aa1a6c521cd9d6874e0f474522fad6a20
Frances Worley <frances@safetytoolbox.co.uk>
2020-04-06 23:11:12 +0100

    convert to angle bracket syntax

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -6,10 +6,8 @@ module.exports = {
     'no-inline-styles': {
       allowDynamicStyles: true
     },
-    'no-curly-component-invocation': false,
     'no-action': false,
     'no-implicit-this': false,
-    'require-button-type': false,
     'link-href-attributes': false,
     'require-valid-alt-text': false,
     'no-invalid-role': false
