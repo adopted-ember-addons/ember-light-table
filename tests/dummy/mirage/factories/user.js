0c404499021dd10126a696f1c4f0cbd2f3393555
maxwondercorn <gmartell@pobox.com>
2022-07-08 13:12:59 -0400

    Revert "fix: Change to @faker/fakerjs to fix missing avatars"
    @faker/fakerjs requires Node 14. This reverts commit 81ba515d4763e2b340e49cc58f018aa9ad8384ad.

--- tests/dummy/mirage/factories/user.js
+++ tests/dummy/mirage/factories/user.js
@@ -4,22 +4,22 @@
   Create more files in this directory to define additional factories.
 */
 import { Factory } from 'ember-cli-mirage';
-import { faker } from '@faker-js/faker';
+import faker from 'faker';
 
 faker.locale = 'en_US';
 
 const MATERIAL_UI_COLORS = ['#F44336', '#E91E63', '#9C27B0', '#009688', '#2196F3', '#4CAF50', '#FFC107', '#FF5722', '#607D8B'];
 
 export default Factory.extend({
-  firstName: () => faker.name.firstName(),
-  lastName: () => faker.name.firstName(),
-  company: () => faker.company.companyName(),
-  address: () => faker.address.streetAddress(),
-  country: () => faker.address.country(),
-  state: () => faker.address.state(),
-  email: () => faker.internet.email(),
-  username: () => faker.internet.userName(),
-  avatar: () => faker.internet.avatar(),
-  bio: () => faker.lorem.paragraph(),
-  color: () => faker.helpers.arrayElement(MATERIAL_UI_COLORS)
+  firstName: faker.name.firstName,
+  lastName: faker.name.firstName,
+  company: faker.company.companyName,
+  address: faker.address.streetAddress,
+  country: faker.address.country,
+  state: faker.address.state,
+  email: faker.internet.email,
+  username: faker.internet.userName,
+  avatar: faker.internet.avatar,
+  bio: faker.lorem.paragraph,
+  color: () => faker.random.arrayElement(MATERIAL_UI_COLORS)
 });
