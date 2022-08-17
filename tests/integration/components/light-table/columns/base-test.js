790c137d6c9a086bea74bd34352ba76e434301d1
maxwondercorn <gmartell@pobox.com>
2022-08-16 23:37:07 -0400

    Add test for column alignment

--- tests/integration/components/light-table/columns/base-test.js
+++ tests/integration/components/light-table/columns/base-test.js
@@ -11,7 +11,46 @@ module('Integration | Component | Columns | base', function (hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });"
     this.set('column', Column.create());
-    await render(hbs`{{light-table/columns/base column=this.column}}`);
+    await render(hbs`<LightTable::Columns::Base @column={{this.column}} />`);
     assert.dom('*').hasText('');
+  });
+
+  test('column alignment', async function (assert) {
+    this.set(
+      'column',
+      Column.create({
+        label: 'First Name',
+        valuePath: 'firstName',
+        align: 'left',
+        width: '150px',
+      })
+    );
+
+    await render(hbs`<LightTable::Columns::Base @column={{this.column}} />`);
+    assert.dom('th').hasClass('align-left');
+
+    this.set(
+      'column',
+      Column.create({
+        label: 'Middle Name',
+        valuePath: 'lastName',
+        align: 'center',
+        width: '150px',
+      })
+    );
+    await render(hbs`<LightTable::Columns::Base @column={{this.column}} />`);
+    assert.dom('th').hasClass('align-center');
+
+    this.set(
+      'column',
+      Column.create({
+        label: 'Last Name',
+        valuePath: 'lastName',
+        align: 'right',
+        width: '150px',
+      })
+    );
+    await render(hbs`<LightTable::Columns::Base @column={{this.column}} />`);
+    assert.dom('th').hasClass('align-right');
   });
 });
